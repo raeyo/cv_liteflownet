2f77ced97c52cdd4e18ff869f666a264cbf9a915
raeyo <raeyo.kang@gmail.com>
2021-12-13 16:39:04 +0000

    backup: LiteFlowNet

--- model.py
+++ model.py
@@ -1,6 +1,13 @@
+'''
+reference: 
+https://github.com/NVIDIA/flownet2-pytorch
+https://github.com/sniklaus/pytorch-liteflownet
+
+'''
 import torch
 import torch.nn as nn
 import sys
+from torchvision.transforms import Resize
 
 #reference: https://github.com/NVIDIA/flownet2-pytorch
 def conv(in_channels, out_channels, kernel_size=3, stride=1, padding=1):
@@ -31,6 +38,7 @@ def backwarp(tenInput, tenFlow):
 
 	return torch.nn.functional.grid_sample(input=tenInput, grid=(backwarp_tenGrid[str(tenFlow.shape)] + tenFlow).permute(0, 2, 3, 1), mode='bilinear', padding_mode='zeros', align_corners=False)
 # end
+#reference: https://github.com/sniklaus/pytorch-liteflownet
 try:
 	from .correlation import correlation # the custom cost volume layer
 except:
@@ -104,9 +112,7 @@ class NetE(nn.Module):
     )
     
     # Regulariation Unit
-    self.R_rm_flow = None
-    self.R_rgbwarp = None
-    self.R_norm = None
+    self.R_rgbwarp = backwarp #reference
     
     self.R_conv = nn.Sequential(
       conv(feature_dim+3, feature_dim, 3, 1),
@@ -120,10 +126,9 @@ class NetE(nn.Module):
       
       conv(feature_dim//4, 9, 3, 1), # conv dist R
     )
-
-    self.R_negsq = None
+    self.unfold = torch.nn.Unfold(kernel_size=3, padding=1)
         
-  def forward(self, f1, f2, prev_flow):
+  def forward(self, f1, f2, prev_flow, im1, im2):
     # Matching Unit
     if prev_flow is None:
       prev_flow = 0.0 #reference: https://github.com/sniklaus/pytorch-liteflownet
@@ -147,9 +152,32 @@ class NetE(nn.Module):
 
     # Regulariation Unit
     
-
+    # remove mean of flow field
+    rm_flow = pred_flow.view(pred_flow.size(0), pred_flow.size(1), -1)
+    rm_flow = rm_flow - rm_flow.mean(dim=2, keepdim=True)
     
-    return pred_flow + prev_flow
+    # Image warp
+    warped_im2 = self.R_rgbwarp(im2, pred_flow)
+    
+    # Brightness error  == occlusion probability
+    O = torch.norm(warped_im2-im1, p=2, dim=1, keepdim=True)
+    
+    # distance metric
+    D = self.R_conv(torch.cat([f1, rm_flow.view_as(pred_flow), O], dim=1)) # B, 9, H, W
+    
+    # filter of f-lcon
+    # reference: https://github.com/sniklaus/pytorch-liteflownet
+    D = -D**2
+    g = torch.nn.functional.softmax(D, dim=1) # B, 9, H, W
+     
+    u_flow = pred_flow[:, 0:1, :, :]
+    v_flow = pred_flow[:, 1:2, :, :]
+    u_flow = torch.sum(self.unfold(u_flow) * g.view(g.size(0), g.size(1), -1), dim=1).view_as(u_flow)
+    v_flow = torch.sum(self.unfold(v_flow) * g.view(g.size(0), g.size(1), -1), dim=1).view_as(v_flow)
+    
+    pred_flow = torch.cat([u_flow, v_flow], dim=1)
+    
+    return pred_flow
 
 class LiteFlowNet(nn.Module):
   
@@ -168,16 +196,23 @@ class LiteFlowNet(nn.Module):
     pyramid_features1 = self.feature_descriptor(I1)
     pyramid_features2 = self.feature_descriptor(I2)
     
+    height = I1.size(2)
+    width = I1.size(3)
+    
     
     '''Cascaded Flow Inference and Flow Regularization'''
     # pyramid features: [F1, F2, F3, F4, F5, F6]
-    # feature dim:      [32, 32, 64, 96, 128, 192]
+    # feature dim:      3 -> [32, 32, 64, 96, 128, 192]
+    # image size(height): 384 -> [384, 192, 96, 48, 24, 12]
     flow = None
     for level in range(6):
       level = 5 - level
+      im1 = Resize((height//(2**level), width//(2**level)))(I1)
+      im2 = Resize((height//(2**level), width//(2**level)))(I2)
+
       f1 = pyramid_features1[level]
       f2 = pyramid_features2[level]
       
-      flow = self.flow_estimator[level](f1, f2, flow)
+      flow = self.flow_estimator[level](f1, f2, flow, im1, im2)
 
     return flow
