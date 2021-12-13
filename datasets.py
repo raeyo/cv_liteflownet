08869f3e2c37aea55c9993bdeef0c22e9c12c143
raeyo <raeyo.kang@gmail.com>
2021-12-13 12:29:50 +0000

    backup: LiteFlowNet_M0

--- datasets.py
+++ datasets.py
@@ -12,103 +12,104 @@ import numpy as np
 from glob import glob
 import utils.frame_utils as frame_utils
 
-class StaticRandomCrop(object):
-	def __init__(self, image_size, crop_size):
-		self.th, self.tw = crop_size
-		h, w = image_size
-		self.h1 = random.randint(0, h - self.th)
-		self.w1 = random.randint(0, w - self.tw)
 
-	def __call__(self, img):
-		return img[self.h1:(self.h1+self.th), self.w1:(self.w1+self.tw),:]
+
+class StaticRandomCrop(object):
+  def __init__(self, image_size, crop_size):
+    self.th, self.tw = crop_size
+    h, w = image_size
+    self.h1 = random.randint(0, h - self.th)
+    self.w1 = random.randint(0, w - self.tw)
+
+  def __call__(self, img):
+    return img[self.h1:(self.h1+self.th), self.w1:(self.w1+self.tw),:]
 
 class StaticCenterCrop(object):
-	def __init__(self, image_size, crop_size):
-		self.th, self.tw = crop_size
-		self.h, self.w = image_size
-	def __call__(self, img):
-		return img[(self.h-self.th)//2:(self.h+self.th)//2, (self.w-self.tw)//2:(self.w+self.tw)//2,:]
+  def __init__(self, image_size, crop_size):
+    self.th, self.tw = crop_size
+    self.h, self.w = image_size
+  def __call__(self, img):
+    return img[(self.h-self.th)//2:(self.h+self.th)//2, (self.w-self.tw)//2:(self.w+self.tw)//2,:]
 
 class MpiSintel(data.Dataset):
-	def __init__(self, args, is_cropped = False, root = '', dstype = 'clean', replicates = 1):
-		self.args = args
-		self.is_cropped = is_cropped
-		self.crop_size = args.crop_size
-		self.render_size = args.inference_size
-		self.replicates = replicates
+  def __init__(self, args, is_cropped=False, root = '', dstype='clean', replicates = 1):
+    self.args = args
+    self.is_cropped = is_cropped
+    self.crop_size = args.crop_size
+    self.render_size = args.inference_size
+    self.replicates = replicates
 
-		flow_root = join(root, 'flow')
-		image_root = join(root, dstype)
+    flow_root = join(root, 'flow')
+    image_root = join(root, dstype)
 
-		file_list = sorted(glob(join(flow_root, '*/*.flo')))
+    file_list = sorted(glob(join(flow_root, '*/*.flo')))
+    self.flow_list = []
+    self.image_list = []
 
-		self.flow_list = []
-		self.image_list = []
+    for file in file_list:
+      if 'test' in file:
+        # print file
+        continue
 
-		for file in file_list:
-			if 'test' in file:
-				# print file
-				continue
+      fbase = file[len(flow_root)+1:]
+      fprefix = fbase[:-8]
+      fnum = int(fbase[-8:-4])
 
-			fbase = file[len(flow_root)+1:]
-			fprefix = fbase[:-8]
-			fnum = int(fbase[-8:-4])
+      img1 = join(image_root, fprefix + "%04d"%(fnum+0) + '.png')
+      img2 = join(image_root, fprefix + "%04d"%(fnum+1) + '.png')
 
-			img1 = join(image_root, fprefix + "%04d"%(fnum+0) + '.png')
-			img2 = join(image_root, fprefix + "%04d"%(fnum+1) + '.png')
+      if not isfile(img1) or not isfile(img2) or not isfile(file):
+        continue
 
-			if not isfile(img1) or not isfile(img2) or not isfile(file):
-				continue
+      # img1 = frame_utils.read_gen(img1)
+      # img2 = frame_utils.read_gen(img2)
+      # flow = frame_utils.read_gen(file)
+      self.image_list.append([img1, img2])
+      self.flow_list.append(file)
 
-			self.image_list += [[img1, img2]]
-			self.flow_list += [file]
+    self.size = len(self.image_list)
 
-		self.size = len(self.image_list)
+    self.frame_size = frame_utils.read_gen(self.image_list[0][0]).shape
 
-		self.frame_size = frame_utils.read_gen(self.image_list[0][0]).shape
+    if (self.render_size[0] < 0) or (self.render_size[1] < 0) or (self.frame_size[0]%64) or (self.frame_size[1]%64):
+      self.render_size[0] = ( (self.frame_size[0])//64 ) * 64
+      self.render_size[1] = ( (self.frame_size[1])//64 ) * 64
 
-		if (self.render_size[0] < 0) or (self.render_size[1] < 0) or (self.frame_size[0]%64) or (self.frame_size[1]%64):
-			self.render_size[0] = ( (self.frame_size[0])//64 ) * 64
-			self.render_size[1] = ( (self.frame_size[1])//64 ) * 64
+    args.inference_size = self.render_size
 
-		args.inference_size = self.render_size
+    
 
-		assert (len(self.image_list) == len(self.flow_list))
+    assert (len(self.image_list) == len(self.flow_list))
 
-	def __getitem__(self, index):
+  def __getitem__(self, index):
 
-		index = index % self.size
+    index = index % self.size
 
-		img1 = frame_utils.read_gen(self.image_list[index][0])
-		img2 = frame_utils.read_gen(self.image_list[index][1])
+    img1 = frame_utils.read_gen(self.image_list[index][0])
+    img2 = frame_utils.read_gen(self.image_list[index][1])
+    flow = frame_utils.read_gen(self.flow_list[index])
 
-		flow = frame_utils.read_gen(self.flow_list[index])
+    # img1 = self.image_list[index][0]
+    # img2 = self.image_list[index][1]
+    # flow = self.flow_list[index]
+    
+    images = [img1, img2]
+    image_size = img1.shape[:2]
 
-		images = [img1, img2]
-		image_size = img1.shape[:2]
+    if self.is_cropped:
+      cropper = StaticRandomCrop(image_size, self.crop_size)
+    else:
+      cropper = StaticCenterCrop(image_size, self.render_size)
+    images = list(map(cropper, images))
+    flow = cropper(flow)
 
-		if self.is_cropped:
-			cropper = StaticRandomCrop(image_size, self.crop_size)
-		else:
-			cropper = StaticCenterCrop(image_size, self.render_size)
-		images = list(map(cropper, images))
-		flow = cropper(flow)
+    images = np.array(images).transpose(3,0,1,2)
+    flow = flow.transpose(2,0,1)
 
-		images = np.array(images).transpose(3,0,1,2)
-		flow = flow.transpose(2,0,1)
+    images = torch.from_numpy(images.astype(np.float32))
+    flow = torch.from_numpy(flow.astype(np.float32))
 
-		images = torch.from_numpy(images.astype(np.float32))
-		flow = torch.from_numpy(flow.astype(np.float32))
+    return images, flow
 
-		return [images], [flow]
-
-	def __len__(self):
-		return self.size * self.replicates
-
-class MpiSintelClean(MpiSintel):
-	def __init__(self, args, is_cropped = False, root = '', replicates = 1):
-		super(MpiSintelClean, self).__init__(args, is_cropped = is_cropped, root = root, dstype = 'clean', replicates = replicates)
-
-class MpiSintelFinal(MpiSintel):
-	def __init__(self, args, is_cropped = False, root = '', replicates = 1):
-		super(MpiSintelFinal, self).__init__(args, is_cropped = is_cropped, root = root, dstype = 'final', replicates = replicates)
+  def __len__(self):
+    return self.size * self.replicates
--- train.py
+++ train.py
@@ -1,6 +1,7 @@
 import argparse
 import cv2
 import numpy as np
+from numpy.core.fromnumeric import trace
 from tqdm import tqdm
 import os
 
@@ -16,7 +17,9 @@ from utils.flow_utils import flow2img
 
 from model import LiteFlowNet
 
-def visualize_result(image1, image2, flow, save_path):
+torch.set_num_threads(4)
+
+def visualize_result(image1, image2, flow, save_path, gt=None):
   image1 = np.uint8(image1.permute(1, 2, 0))
   image1 = cv2.cvtColor(image1, cv2.COLOR_BGR2RGB)
   image1 = cv2.putText(image1.copy(), "Image 1", (30, 30), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 0, 0), 2, cv2.LINE_AA)
@@ -25,12 +28,20 @@ def visualize_result(image1, image2, flow, save_path):
   image2 = cv2.cvtColor(image2, cv2.COLOR_BGR2RGB)
   image2 = cv2.putText(image2.copy(), "Image 2", (30, 30), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 0, 0), 2, cv2.LINE_AA)
 
-  flow = flow.permute(1, 2, 0).numpy()
+  flow = flow.permute(1, 2, 0).detach().numpy()
   flow = flow2img(flow)
   flow = cv2.cvtColor(flow, cv2.COLOR_BGR2RGB)
   flow = cv2.putText(flow.copy(), "Flow", (30, 30), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 0, 0), 2, cv2.LINE_AA)
 
-  cv2.imwrite(save_path, np.vstack([image1, image2, flow]))
+  if gt is None:
+    cv2.imwrite(save_path, np.vstack([image1, image2, flow]))
+  else:
+    gt = gt.permute(1, 2, 0).numpy()
+    gt = flow2img(gt)
+    gt = cv2.cvtColor(gt, cv2.COLOR_BGR2RGB)
+    gt = cv2.putText(gt.copy(), "GT", (30, 30), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 0, 0), 2, cv2.LINE_AA)
+    cv2.imwrite(save_path, np.vstack([image1, image2, flow, gt]))
+
 
 
 if __name__=="__main__":
@@ -44,12 +55,12 @@ if __name__=="__main__":
 
   '''train arguments'''
   parser.add_argument('--batch_size', type=int, 
-    default = 4, help='batch size for training')
+    default = 32, help='batch size for training')
   parser.add_argument('--maximum_epoch', type=int, 
     default = 100, help='maximum epoch')
   parser.add_argument('--test_interval', type=int, 
     default = 5, help='test interval(epoch) while training')
-  parser.add_argument('--gpu', default="0", help='gpu id')
+  parser.add_argument('--gpu', default="6, 7", help='gpu id')
 
 
   '''logging argument'''
@@ -57,8 +68,6 @@ if __name__=="__main__":
     default='/data2/raeyo/results/liteflownet_M0', help='logging dir')
   parser.add_argument('--neptune', 
     action='store_true', help='neptune logging')
-
-  
 
   args = parser.parse_args()
   if not os.path.isdir(args.log_dir):
@@ -76,29 +85,25 @@ if __name__=="__main__":
       api_token="eyJhcGlfYWRkcmVzcyI6Imh0dHBzOi8vYXBwLm5lcHR1bmUuYWkiLCJhcGlfdXJsIjoiaHR0cHM6Ly9hcHAubmVwdHVuZS5haSIsImFwaV9rZXkiOiI0OTA1Mzk4OS04MWI4LTQ5YjctYTViZi1iZDEyNjFlOWJmMzAifQ==",
       )
 
-    
-
   '''load dataset'''
-  train_root = "/data2/raeyo/optical_flow/MPI-Sintel/training"
-  test_root = "/data2/raeyo/optical_flow/MPI-Sintel/training"
-
-  train_dataset = MpiSintel(args, is_cropped=True, root=train_root)
-  train_loader = DataLoader(train_dataset, batch_size=args.batch_size, shuffle=True, num_workers=4)
-
-  test_dataset = MpiSintel(args, is_cropped=True, dstype='final', root=test_root)
-  test_loader = DataLoader(test_dataset, batch_size=args.batch_size, shuffle=False, num_workers=4)
+  data_root = "/data2/raeyo/optical_flow/MPI-Sintel/training"
+  
+  train_dataset = MpiSintel(args, is_cropped=True, root=data_root)
+  train_loader = DataLoader(train_dataset, batch_size=args.batch_size, shuffle=True, num_workers=0)
 
   '''data example'''
   images, flow = train_dataset[0]
-
-  image1 = images[0][:, 0, :, :]
-  image2 = images[0][:, 1, :, :]
-  flow = flow[0]
+  
+  image1 = images[:, 0, :, :]
+  image2 = images[:, 1, :, :]
   visualize_result(image1, image2, flow, "sample_data.png")
 
   '''load Model'''
   model = LiteFlowNet()
   device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+  if torch.cuda.device_count() > 1:
+    print("Using {} gpu!".format(torch.cuda.device_count()))
+    model = nn.DataParallel(model)
   model = model.to(device)
 
   '''optimizer'''
@@ -115,9 +120,9 @@ if __name__=="__main__":
     losses = []
     
     for itr, data in enumerate(tqdm(train_loader)):
-      input_image1 = data[0][0][:, :, 0, :, :].cuda() # --> B, 3, 384, 1024
-      input_image2 = data[0][0][:, :, 1, :, :].cuda() # --> B, 3, 384, 1024
-      target_flow = data[1][0].cuda() # --> B, 2, 384, 1024
+      input_image1 = data[0][:, :, 0, :, :].cuda() # --> B, 3, 384, 1024
+      input_image2 = data[0][:, :, 1, :, :].cuda() # --> B, 3, 384, 1024
+      target_flow = data[1].cuda() # --> B, 2, 384, 1024
 
       pred_flow = model(input_image1, input_image2)
       
@@ -132,32 +137,13 @@ if __name__=="__main__":
       
     train_loss = np.mean(losses)
     print("[{}/{}] train loss | {:<7.3f}".format(epoch, args.maximum_epoch, train_loss))
-
+    if train_loss < best_loss:
+      best_loss = train_loss
+      best_epoch = epoch
+      torch.save(model.state_dict(), os.path.join(args.log_dir, "weights/best.pkl"))
     '''evaluation loop'''   
     if epoch % args.test_interval == 0:
-      with torch.no_grad():
-        model = model.eval()
-        losses = []
-        
-        for itr, data in tqdm(enumerate(test_loader)):
-          input_image1 = data[0][0][:, :, 0, :, :].cuda() # --> B, 3, 384, 1024
-          input_image2 = data[0][0][:, :, 1, :, :].cuda() # --> B, 3, 384, 1024
-          target_flow = data[1][0].cuda() # --> B, 2, 384, 1024
-          
-          pred_flow = model(input_image1, input_image2)
-    
-          loss, epe = criterion(pred_flow, target_flow)
-          losses.append(loss.item())
-          
-        save_path = os.path.join(args.log_dir, "pred", "{}_{}.png".format(epoch, itr))
-        visualize_result(input_image1[0].cpu(), input_image2[0].cpu(), pred_flow[0].cpu(), save_path)
-        test_loss = np.mean(losses)
-        print("[{}/{}] test loss | {:<7.3f}".format(epoch, args.maximum_epoch, test_loss))
-        
-        torch.save(model.state_dict(), os.path.join(args.log_dir, "weights/epoch{}.pkl".format(epoch)))
-        if test_loss < best_loss:
-          best_loss = test_loss
-          torch.save(model.state_dict(), os.path.join(args.log_dir, "weights/best.pkl"))
-        if args.neptune:
-          nlogger["test/loss"].log(loss.item())
-            
+      save_path = os.path.join(args.log_dir, "pred", "{}_{}.png".format(epoch, itr))
+      visualize_result(input_image1[0].cpu(), input_image2[0].cpu(), pred_flow[0].cpu(), save_path, gt=target_flow[0].cpu())
+      torch.save(model.state_dict(), os.path.join(args.log_dir, "weights/epoch{}.pkl".format(epoch)))
+  
